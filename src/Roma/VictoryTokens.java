f1817368a1429f3a20559145958d08256603063d
lukylemmie <lemmie6@gmail.com>
2012-04-11 17:41:04 +1000

    Merge branch 'lem'
    Conflicts:
    	src/Roma/VictoryTokens.java

--- src/Roma/VictoryTokens.java
+++ src/Roma/VictoryTokens.java
@@ -66,4 +66,8 @@ public class VictoryTokens {
         return "Player One has: " + playerTokens[Roma.PLAYER_ONE] + " tokens, and Player Two has: "+
                 playerTokens[Roma.PLAYER_TWO] + " Tokens";
 	}
+
+    public void cry(){
+        System.out.println("wtf");
+    }
 }

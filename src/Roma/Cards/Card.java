988468acab42c2b07add0adedb91d047d10c82de
Varun Nayyar <nayyarv@gmail.com>
2012-04-17 14:43:08 +1000

    Testing

--- src/Roma/Cards/Card.java
+++ src/Roma/Cards/Card.java
@@ -1,56 +1,50 @@
 package Roma.Cards;
-
 import Roma.*;
 
 public abstract class Card {
-    private final String name;
-    private final String type;
-    private final String description;
-    private final int cost;
-    private final int defence;
-    private final PlayArea playArea;
+	private final String name;
+	private final String type;
+	private final String description;
+	private final int cost;
+	private final int defence;
+	private final PlayArea playArea;
     private boolean playable = false;
+	
+	public Card(String name, String type, String description, int cost, int defense, PlayArea playArea){
+		this.name = name;
+		this.type = type;
+		this.description = description;
+		this.cost = cost;
+		this.defence = defense;
+		this.playArea = playArea;
+	}
+	
+	public String getName() {
+		return name;
+	}
 
-    public Card(String name, String type, String description, int cost, int defense, PlayArea playArea) {
-        this.name = name;
-        this.type = type;
-        this.description = description;
-        this.cost = cost;
-        this.defence = defense;
-        this.playArea = playArea;
-    }
+	public String getType() {
+		return type;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getDescription() {
+		return description;
+	}
 
+	public int getCost() {
+		return cost;
+	}
 
-    public String getType() {
-        return type;
-    }
+	public int getDefense() {
+		return defence;
+	}
 
+	public PlayArea getPlayArea() {
+		return playArea;
+	}
 
-    public String getDescription() {
-        return description;
-    }
-
-
-    public int getCost() {
-        return cost;
-    }
-
-
-    public int getDefense() {
-        return defence;
-    }
-
-
-    public PlayArea getPlayArea() {
-        return playArea;
-    }
-
-    public String toString() {
-        return "Card Name: " + name + "; Type: " + type + "\nDescription: " + description + "\nCost: "
+    public String toString(){
+        return  "Card Name: " + name + "; Type: "+ type+"\nDescription: " + description + "\nCost: "
                 + cost + "; Defence: " + defence;
 
     }
@@ -63,5 +57,5 @@ public abstract class Card {
         this.playable = playable;
     }
 
-    public abstract void activate(int player);
+	public abstract void activate(int player);
 }

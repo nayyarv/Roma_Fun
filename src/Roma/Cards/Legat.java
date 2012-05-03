33305cf4397324ff6ae375f56ce60aa1083d606b
Andrew Lem <lemmie6@gmail.com>
2012-05-03 11:51:09 +1000

    Legat implemented

--- src/Roma/Cards/Legat.java
+++ src/Roma/Cards/Legat.java
@@ -29,6 +29,19 @@ public class Legat extends Card {
 
     public boolean activate(Player player, int position) {
         boolean activated = true;
+        DiceDiscs diceDiscs = playArea.getDiceDiscs();
+        VictoryTokens victoryTokens = playArea.getVictoryTokens();
+        int targetPlayerID = (player.getPlayerID() + 1) % Roma.MAX_PLAYERS;
+        Card[] enemyCards = diceDiscs.getPlayerActives(targetPlayerID);
+        int emptySlotCount = 0;
+
+        for(Card card : enemyCards){
+            if(card == null){
+                emptySlotCount++;
+            }
+        }
+
+        victoryTokens.playerFromPool(player.getPlayerID(), emptySlotCount);
 
         return activated;
     }

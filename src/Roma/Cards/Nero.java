fde68ebdeccca118527430e1541fffd8bb66a87a
Andrew Lem <lemmie6@gmail.com>
2012-05-03 12:26:58 +1000

    Nero hacked together similar to Gladiator

--- src/Roma/Cards/Nero.java
+++ src/Roma/Cards/Nero.java
@@ -2,6 +2,9 @@ package Roma.Cards;
 
 import Roma.*;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * File Name:
  * Creator: Varun Nayyar
@@ -27,7 +30,40 @@ public class Nero extends Card {
 
 
     public boolean activate(Player player, int position) {
+        //TODO: refactor input to interface or player
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> validInput = new ArrayList<Integer>();
+        boolean inputValid = false;
+        int chosenInput = -1;
+
         boolean activated = true;
+        DiceDiscs diceDiscs = playArea.getDiceDiscs();
+        int targetPlayerID = (player.getPlayerID() + 1) % Roma.MAX_PLAYERS;
+
+        Card[] enemyCards = diceDiscs.getPlayerActives(targetPlayerID);
+
+        System.out.println("Available Targets:");
+        for(int i = 0; i < enemyCards.length; i++){
+            if(enemyCards[i].getType() == Card.BUILDING){
+                System.out.println((i + 1) + ") " + enemyCards[i].getName());
+                validInput.add(i + 1);
+            } else {
+                System.out.println((i + 1) + ") #");
+            }
+        }
+
+        while(!inputValid){
+            chosenInput = input.nextInt();
+            for(int number : validInput){
+                if(chosenInput == number){
+                    inputValid = true;
+                }
+            }
+        }
+        chosenInput--;
+
+        diceDiscs.discardTarget(targetPlayerID, chosenInput);
+        diceDiscs.discardTarget(player.getPlayerID(), position);
 
         return activated;
     }

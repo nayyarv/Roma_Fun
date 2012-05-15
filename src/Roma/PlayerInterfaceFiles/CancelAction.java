308f30c3790754d67574e22018237687e5f77c89
Lem <lemmie6@gmail.com>
2012-05-16 09:44:53 +1000

    refactoring player with cancel exception thrown

---
+++ src/Roma/PlayerInterfaceFiles/CancelAction.java
@@ -0,0 +1,14 @@
+package Roma.PlayerInterfaceFiles;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Andrew
+ * Date: 16/05/12
+ * Time: 9:36 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CancelAction extends Exception {
+    public void message(){
+        System.out.println("Cancelling Action");
+    }
+}

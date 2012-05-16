94ef5ce0207d86af05058aa81602d86cb37fbe2e
Varun Nayyar <nayyarv@gmail.com>
2012-05-16 16:40:21 +1000

    Minor CHanges

--- src/Roma/PlayerInterfaceFiles/CancelAction.java
+++ src/Roma/PlayerInterfaceFiles/CancelAction.java
@@ -1,11 +1,10 @@
 package Roma.PlayerInterfaceFiles;
 
 /**
- * Created with IntelliJ IDEA.
- * User: Andrew
+ * Date: Cancel Action Exp
+ * User: Andrew & Varun
  * Date: 16/05/12
  * Time: 9:36 AM
- * To change this template use File | Settings | File Templates.
  */
 public class CancelAction extends Exception {
     public void message(){

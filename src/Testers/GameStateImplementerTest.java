090592eb010dd4c922c96e9770d31da1d7b8fa1e
Varun Nayyar <nayyarv@gmail.com>
2012-05-19 14:09:07 +1000

    Adding more testing

--- src/Testers/GameStateImplementerTest.java
+++ src/Testers/GameStateImplementerTest.java
@@ -1,6 +1,7 @@
 package Testers;
 
 import Implementers.GameStateImplementer;
+import framework.Rules;
 import framework.cards.Card;
 import junit.framework.TestCase;
 
@@ -25,6 +26,7 @@ public class GameStateImplementerTest extends TestCase {
         assert (gameStateImplementer.getWhoseTurn()==0);
         gameStateImplementer.setWhoseTurn(1);
         assert (gameStateImplementer.getWhoseTurn()==1);
+        System.out.println("Turn functions passed!!\n");
     }
 
     public void testSetDeck() throws Exception {
@@ -42,31 +44,63 @@ public class GameStateImplementerTest extends TestCase {
         System.out.println("Cardlist size = "+cardList.size()+" deck size = "+deck.size());
 
         assert (deck.size()==cardList.size());
+        System.out.println("Deck functions passed!!!\n");
 
     }
 
-    public void testGetDiscard() throws Exception {
-
-    }
 
     public void testSetDiscard() throws Exception {
+        //TODO: check top card in discard pile
+        System.out.println("Testing testDiscard and getDiscard");
+        List<Card> cardList= new ArrayList<Card>();
+        Collections.addAll(cardList, Card.values());
+        cardList.remove(Card.GRIMREAPER);
+        cardList.remove(Card.TELEPHONEBOX);
+        cardList.remove(Card.KAT);
+
+        gameStateImplementer.setDiscard(cardList);
+
+        List<Card> deck = gameStateImplementer.getDiscard();
+
+        System.out.println("Cardlist size = "+cardList.size()+" deck size = "+deck.size());
+
+        assert (deck.size()==cardList.size());
+        System.out.println("Discard functions passed!!!\n");
 
     }
 
-    public void testGetPlayerSestertii() throws Exception {
+
+
+    public void testPlayerSestertii() throws Exception {
+        gameStateImplementer.setPlayerSestertii(0,10);
+        assert (gameStateImplementer.getPlayerSestertii(0)==10);
+        assert !(gameStateImplementer.getPlayerSestertii(0)==100);
+        gameStateImplementer.setPlayerSestertii(0,100);
+        assert (gameStateImplementer.getPlayerSestertii(0)==100);
+        assert !(gameStateImplementer.getPlayerSestertii(0)==10);
+
+        gameStateImplementer.setPlayerSestertii(1,1000);
+        assert (gameStateImplementer.getPlayerSestertii(1)==1000);
+        assert !(gameStateImplementer.getPlayerSestertii(1)==10);
+        assert !(gameStateImplementer.getPlayerSestertii(1)==100);
+
+        gameStateImplementer.setPlayerSestertii(1,10020);
+        assert (gameStateImplementer.getPlayerSestertii(1)==10020);
+        System.out.println("Money functions passed!!!\n");
 
     }
 
-    public void testSetPlayerSestertii() throws Exception {
+    public void testPlayerVictoryPoints() throws Exception {
+        System.out.println("Testing Victory Points");
 
-    }
+        System.out.println(gameStateImplementer.getPlayerVictoryPoints(0));
+        assert (gameStateImplementer.getPlayerVictoryPoints(1)==10);
+        assert (gameStateImplementer.getPoolVictoryPoints()==16);
+        gameStateImplementer.setPlayerVictoryPoints(0, 5);
 
-    public void testGetPlayerVictoryPoints() throws Exception {
 
-    }
 
-    public void testSetPlayerVictoryPoints() throws Exception {
-
+        System.out.println("VP functions passed!!!\n");
     }
 
     public void testGetPlayerHand() throws Exception {
@@ -86,10 +120,6 @@ public class GameStateImplementerTest extends TestCase {
     }
 
     public void testGetActionDice() throws Exception {
-
-    }
-
-    public void testGetPoolVictoryPoints() throws Exception {
 
     }
 

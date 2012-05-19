20e3499ee78d80c20a7c72abd5c6c1c42182bba2
Varun Nayyar <nayyarv@gmail.com>
2012-05-19 17:45:00 +1000

    Implementing a cat tester, some slight fixes

--- src/Testers/TestingCat.java
+++ src/Testers/TestingCat.java
@@ -13,12 +13,14 @@ public class TestingCat {
 
     public static void main(String []args){
         GameStateImplementer gameStateImplementer = new GameStateImplementer();
-
+        int[] num = {1,1,1};
         gameStateImplementer.setWhoseTurn(0);
-        Card[] diceDiscs = {Card.KAT, Card.GRIMREAPER, Card.NOT_A_CARD, Card.NOT_A_CARD,
+        gameStateImplementer.setActionDice(num);
+        Card[] diceDiscs = {Card.KAT, Card.CONSUL, Card.NOT_A_CARD, Card.NOT_A_CARD,
                 Card.NOT_A_CARD, Card.NOT_A_CARD, Card.NOT_A_CARD};
         gameStateImplementer.setPlayerCardsOnDiscs(0, diceDiscs);
         gameStateImplementer.printStats();
+        gameStateImplementer.runGame();
 
     }
 

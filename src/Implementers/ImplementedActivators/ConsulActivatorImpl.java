211aa6ec7d72b2af97892a3006131b62acb0f6de
Varun Nayyar <nayyarv@gmail.com>
2012-05-21 01:18:36 +1000

    Consul Implementation

--- src/Implementers/ImplementedActivators/ConsulActivatorImpl.java
+++ src/Implementers/ImplementedActivators/ConsulActivatorImpl.java
@@ -1,7 +1,10 @@
 package Implementers.ImplementedActivators;
 
+import Roma.Dice;
 import Roma.Player;
 import framework.interfaces.activators.ConsulActivator;
+
+import java.util.ArrayList;
 
 /**
  * File Name:
@@ -9,10 +12,14 @@ import framework.interfaces.activators.ConsulActivator;
  * Date: 21/05/12
  * Desc:
  */
-public class ConsulActivatorImpl extends simpleActivator implements ConsulActivator {
+public class ConsulActivatorImpl  implements ConsulActivator {
+
+    private Player player;
+    private int dieIndex;
+    private int changeAmount;
 
     public ConsulActivatorImpl(Player player) {
-        super(player);
+        this.player = player;
     }
 
     /**
@@ -26,11 +33,42 @@ public class ConsulActivatorImpl extends simpleActivator implements ConsulActiva
      */
     @Override
     public void chooseConsulChangeAmount(int amount) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        changeAmount = amount;
     }
 
     @Override
     public void chooseWhichDiceChanges(int originalRoll) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        //get the die index
+        ArrayList<Dice> freeDice = player.getFreeDice();
+        int currActionIndex = player.getCurrentAction().getActionDiceIndex();
+
+        freeDice.remove(currActionIndex);
+
+
+        Dice dice = freeDice.get(0);
+        int i;
+        for(i=0; i<freeDice.size()&&dice.getValue()!=originalRoll;i++){
+            dice = freeDice.get(i);
+        }
+        dieIndex = i;
+
+    }
+
+    /**
+     * Mark the pending activation as complete.
+     * <p/>
+     * <p>
+     * This method must be called when an activation is complete.
+     * This method cannot be called until all required activation
+     * methods have been called. No other methods in the move maker can
+     * be called after a CardActivator has been received until after its
+     * complete method is called. This is really important.
+     * </p>
+     */
+    @Override
+    public void complete() {
+        player.getActivationData().add(dieIndex);
+        player.getActivationData().add(changeAmount);
+        player.performActions();
     }
 }
